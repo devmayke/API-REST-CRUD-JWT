f1227e0a2cb54b1ba419478ce0922dd792e224e3
Mayke Ken <devmayke@gmail.com>
2021-04-07 18:49:03 -0300

    refatorado

--- midlewares/auth.js
+++ midlewares/auth.js
@@ -1,57 +1,39 @@
 const JWT = require("jsonwebtoken");
 const JWTSecret = "lhgpGKJhb%!=H";
 const UserAPI = require("../models/UserAPI")
-
+const unauthorized =
+{
+    msg:"NOT AUTHORIZED!",
+    miniDocAPI:{
+        InfoRegister:{
+            linkAuthorization:"http://127.0.0.1:3000/register",
+            formatRegister:{
+                name: "xxxxxxxxxxx",
+                email: "xxxxxxxxxx",
+                password: "xxxxxxx"
+            }
+        },
+        InfoAuth:{
+            linkAuth:"http://127.0.0.1:3000/auth",
+            formatAuth:{
+                email: "xxxxxxxxxx",
+                password: "xxxxxxx"
+            }
+        }
+    }
+}
 
 
 module.exports = (req, res, next)=>{
     if(req.path !== "/auth" && req.path !== "/register"){
         const authToken = req.headers['authorization'];
         if(!authToken){
-            res.status(401).json({
-                msg:"NOT AUTHORIZED!",
-                miniDocAPI:{
-                    InfoRegister:{
-                        linkAuthorization:"http://127.0.0.1:3000/register",
-                        formatRegister:{
-                            name: "xxxxxxxxxxx",
-                            email: "xxxxxxxxxx",
-                            password: "xxxxxxx"
-                        }
-                    },
-                    InfoAuth:{
-                        linkAuth:"http://127.0.0.1:3000/auth",
-                        formatAuth:{
-                            email: "xxxxxxxxxx",
-                            password: "xxxxxxx"
-                        }
-                    }
-                }
-            })
+            res.status(401).json(unauthorized)
         }
         const token = authToken.split(" ")[1];   
         JWT.verify(token, JWTSecret, (err, data)=>{
             if(err){
-                res.status(401).json({
-                    msg:"NOT AUTHORIZED!",
-                    miniDocAPI:{
-                        InfoRegister:{
-                            linkAuthorization:"http://127.0.0.1:3000/register",
-                            formatRegister:{
-                                name: "xxxxxxxxxxx",
-                                email: "xxxxxxxxxx",
-                                password: "xxxxxxx"
-                            }
-                        },
-                        InfoAuth:{
-                            linkAuth:"http://127.0.0.1:3000/auth",
-                            formatAuth:{
-                                email: "xxxxxxxxxx",
-                                password: "xxxxxxx"
-                            }
-                        }
-                    }
-                })
+                res.status(401).json(unauthorized)
             }else{
                 console.log(data)
                 var {email} = data;
